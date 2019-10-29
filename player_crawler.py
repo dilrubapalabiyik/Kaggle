4c748e3d9bb2ff544dc9565a7c7d0b8988a8cd57
qiaoqiao <you@example.com>
2019-10-29 15:19:36 -0500

    players files added

--- player_crawler.py
+++ player_crawler.py
@@ -8,14 +8,6 @@ import glob
 
 
 # a bunch of functions 
-class choose_level:
-    team_url = 'https://www.pbpstats.com/totals/nba/team' 
-   
-    player_url = 'https://www.pbpstats.com/totals/nba/player'
-    
-    def level(self,new_level):
-    	self.team_url = new_level
-        
 
 def choose_per_game(driver,url,season,season_type):
 
@@ -60,12 +52,12 @@ def get_scoring(driver,url,season,season_type):
 	this is for getting csv for "scoring" category
 	"""
 
-	choose_per_game(driver,team_url,season,season_type)
+	choose_per_game(driver,player_url,season,season_type)
 	download_link_scoring = driver.find_element(By.XPATH,"//a[@download='pbpstats_export.csv']")
 	download_link_scoring.click()
 	time.sleep(1)
-	os.rename('pbpstats_export.csv','team_scoring_'+season+'.csv')
-	process_csv('team_scoring_'+season+'.csv',season,season_type)
+	os.rename('pbpstats_export.csv','player_scoring_'+season+'.csv')
+	process_csv('player_scoring_'+season+'.csv',season,season_type)
 
 
 def get_assists(driver,url,season,season_type):
@@ -84,8 +76,8 @@ def get_assists(driver,url,season,season_type):
 	download_link_assists = driver.find_element(By.XPATH,"//a[@download='pbpstats_export.csv']")
 	download_link_assists.click()
 	time.sleep(1)
-	os.rename('pbpstats_export.csv', 'team_assists_'+season+'.csv')
-	process_csv('team_assists_'+season+'.csv',season,season_type)
+	os.rename('pbpstats_export.csv', 'player_assists_'+season+'.csv')
+	process_csv('player_assists_'+season+'.csv',season,season_type)
 
 
 def get_rebounds(driver,url,season,season_type):
@@ -94,7 +86,7 @@ def get_rebounds(driver,url,season,season_type):
 	this is for getting csv for "Rebounds" category
 	"""
 
-	choose_per_game(driver,team_url,season,season_type)
+	choose_per_game(driver,player_url,season,season_type)
 	table_data_dropdown = driver.find_element(By.XPATH,"//main/div[4]/div[@class='row']/div[@class='col-md-2'][2]/div/div[@tabindex='0']/div[@class='multiselect__select']")
 	table_data_dropdown.click()
 	time.sleep(1)
@@ -104,8 +96,8 @@ def get_rebounds(driver,url,season,season_type):
 	download_link_rebounds = driver.find_element(By.XPATH,"//a[@download='pbpstats_export.csv']")
 	download_link_rebounds.click()
 	time.sleep(1)
-	os.rename('pbpstats_export.csv', 'team_rebounds_'+season+'.csv')
-	process_csv('team_rebounds_'+season+'.csv',season,season_type)
+	os.rename('pbpstats_export.csv', 'player_rebounds_'+season+'.csv')
+	process_csv('player_rebounds_'+season+'.csv',season,season_type)
 
 def process_csv(file_name,season,season_type):
 	"""
@@ -126,57 +118,34 @@ if __name__ == "__main__":
 	print("here!")
 	chrome_options = webdriver.ChromeOptions()
 
-	download_dir = "/home/qiaoqiao/Desktop/CSV" # change this to dir where you want to put your data
+	download_dir = "/home/qiaoqiao/Desktop/CSV/seperated_csvs" # change this to dir where you want to put your data
 	os.chdir(download_dir) 
 	prefs = {'download.default_directory' : download_dir} # setting download directory
 	chrome_options.add_experimental_option('prefs', prefs)  
 
-	driver = webdriver.Chrome('/home/qiaoqiao/Desktop/CSV/chromedriver',chrome_options=chrome_options) # load Chrome driver here
-	choose_level(team_url)
-	
-	# year_list = ['2009-10','2010-11','2011-12','2012-13','2013-14','2014-15','2015-16','2016-17','2017-18','2018-19']
+	driver = webdriver.Chrome('/home/qiaoqiao/Desktop/CSV/chromedriver',options=chrome_options) # load Chrome driver here
+	player_url = 'https://www.pbpstats.com/totals/nba/player' # initial page url
 
-	year_list = ['2009-10','2010-11']
+	year_list = ['2009-10','2010-11','2011-12','2012-13','2013-14','2014-15','2015-16','2016-17','2017-18','2018-19']
+
+	# year_list = ['2009-10','2010-11']
 
 	for n in year_list: 
 
-		get_scoring(driver,team_url,n,"regular season")
-		get_assists(driver,team_url,n,"regular season")
-		get_rebounds(driver,team_url,n,"regular season")
+		get_scoring(driver,player_url,n,"regular season")
+		get_assists(driver,player_url,n,"regular season")
+		get_rebounds(driver,player_url,n,"regular season")
 
 	# file concatenations
 
 	scoring_files = [s for s in glob.glob('*scoring*.csv')]
 	combined_scoring_csv = pd.concat([pd.read_csv(s) for s in scoring_files ])
-	combined_scoring_csv.to_csv("Team_Scoring.csv",index=False)
+	combined_scoring_csv.to_csv("Player_Scoring.csv",index=False)
 
 	assists_files = [s for s in glob.glob('*assists*.csv')]
 	combined_assists_csv = pd.concat([pd.read_csv(s) for s in assists_files ])
-	combined_assists_csv.to_csv("Team_Assists.csv",index=False)
+	combined_assists_csv.to_csv("Player_Assists.csv",index=False)
 
 	rebounds_files = [s for s in glob.glob('*rebounds*.csv')]
 	combined_rebounds_csv = pd.concat([pd.read_csv(s) for s in rebounds_files ])
-	combined_rebounds_csv.to_csv("Team_Rebounds.csv",index=False)
-
-	choose_level(player_url)
-	year_list = ['2009-10','2010-11']
-
-	for n in year_list: 
-
-		get_scoring(driver,team_url,n,"regular season")
-		get_assists(driver,team_url,n,"regular season")
-		get_rebounds(driver,team_url,n,"regular season")
-
-	# file concatenations
-
-	scoring_files = [s for s in glob.glob('*scoring*.csv')]
-	combined_scoring_csv = pd.concat([pd.read_csv(s) for s in scoring_files ])
-	combined_scoring_csv.to_csv("Team_Scoring.csv",index=False)
-
-	assists_files = [s for s in glob.glob('*assists*.csv')]
-	combined_assists_csv = pd.concat([pd.read_csv(s) for s in assists_files ])
-	combined_assists_csv.to_csv("Team_Assists.csv",index=False)
-
-	rebounds_files = [s for s in glob.glob('*rebounds*.csv')]
-	combined_rebounds_csv = pd.concat([pd.read_csv(s) for s in rebounds_files ])
-	combined_rebounds_csv.to_csv("Team_Rebounds.csv",index=False)
+	combined_rebounds_csv.to_csv("Player_Rebounds.csv",index=False)
